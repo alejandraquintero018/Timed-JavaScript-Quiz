75b7924e8a8be1a83cae902b6518559154893295
Alejandra Quintero <alejandraquintero@Allies-MacBook-Pro.local>
2022-08-01 22:35:37 -0700

    added a function to set user input to local storage

--- script.js
+++ script.js
@@ -16,10 +16,46 @@ var answerEl = document.querySelector(".answer");
 var CurrentQuestion = document.querySelector(".Question");
 var EndGameEl = document.querySelector(".EndScript");
 var UserInput = document.querySelector(".Name");
+var SubmitBtn = document.querySelector(".Submit"); 
 var timer = 5;
 var quizinterval;
 var currentquestionindex = 0;
 var index = 0;
+
+var questions = [
+    {
+        title: 'Commonly used data types DO NOT include:',
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts',
+    },
+    {
+        title: 'The condition in an if / else statement is enclosed within ____.',
+        choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+        answer: 'parentheses',
+    },
+    {
+        title: 'Arrays in JavaScript can be used to store ____.',
+        choices: [
+            'numbers and strings',
+            'other arrays',
+            'booleans',
+            'all of the above',
+        ],
+        answer: 'all of the above',
+    },
+    {
+        title:
+            'String values must be enclosed within ____ when being assigned to variables.',
+        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+        answer: 'quotes',
+    },
+    {
+        title:
+            'A very useful tool used during development and debugging for printing content to the debugger is:',
+        choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+        answer: 'console.log',
+    },
+];
 
 //WHEN I click the start button
 //THEN a timer starts  I am presented with a question, this also hides the start page 
@@ -35,14 +71,13 @@ function countdown() {
     if (timer >= 1) {
         timer--;
         timerEl.textContent = "Time: " + timer;
-    }
-    else {
+    } if (timer === 0) {
         quizend();
-    };
+    }
 };
-
 //displaying the current question and populating the HTML with the answers 
 var displayquestions = function () {
+    console.log(questions[index].title);
     CurrentQuestion.textContent = questions[index].title;
     answerEl.innerHTML = "";
     for (var i = 0; i < questions[index].choices.length; i++) {
@@ -50,7 +85,7 @@ var displayquestions = function () {
         var choiceBtn = document.createElement("button");
         choiceBtn.textContent = choice;
         choiceBtn.setAttribute("value", choice);
-        choiceBtn.onclick = answercheck;
+        choiceBtn.onclick = answercheck();
         answerEl.append(choiceBtn);
     };
 };
@@ -58,60 +93,46 @@ var displayquestions = function () {
 //checking the answer the user gives and making an adjustment to the timer
 var answercheck = function () {
     if (this.value !== questions[index].answer) {
-        timer -= 5;
-    } if (this.value === question[index].answer) {
-        displayquestions();
+            index++; 
+            displayquestions();
+            console.log("click");
+            timer -= 5;
+    };
+    if (this.value === questions[index].answer) {
+            index++;
+            displayquestions();
+            console.log("click");
     }
     if (timer === 0) {
         quizend();
     }
 }
-    //function that is called that will prompt the end of the game 
-    //quizend stop the timer 
-    var quizend = function () {
-        clearInterval(quizinterval);
-        document.querySelector(".Quiz").setAttribute("class", "hide");
-        EndGameEl.textContent = "You Ran Out of Time! Try Again?";
-        var Input = document.createElement("input");
-        UserInput.append(Input);
+//function that is called that will prompt the end of the game 
+//quizend stop the timer 
 
-    }
+var quizend = function () {
+    clearInterval(quizinterval);
+    document.querySelector(".Quiz").setAttribute("class", "hide");
+    EndGameEl.textContent = "You Ran Out of Time! Try Again?";
+    //Creating HTML elements that will be the input for name and button to submit.  
+    var Input = document.createElement("input");
+    var saveBtn = document.createElement("button"); 
+    saveBtn.textContent = "Submit"; 
+    //setting the button up to submut then store the name in local storage. 
+    saveBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+    //Grabbing the input value to then store it
+        console.log(Input.value); 
+        localStorage.setItem("name", Input.value);  
+    });
+    UserInput.append("Name: ");
+    UserInput.append(Input);
+    SubmitBtn.append(saveBtn);
+}
 
-    //array that holds all of the questions, choices and answer
-    var questions = [
-        {
-            title: 'Commonly used data types DO NOT include:',
-            choices: ['strings', 'booleans', 'alerts', 'numbers'],
-            answer: 'alerts',
-        },
-        {
-            title: 'The condition in an if / else statement is enclosed within ____.',
-            choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-            answer: 'parentheses',
-        },
-        {
-            title: 'Arrays in JavaScript can be used to store ____.',
-            choices: [
-                'numbers and strings',
-                'other arrays',
-                'booleans',
-                'all of the above',
-            ],
-            answer: 'all of the above',
-        },
-        {
-            title:
-                'String values must be enclosed within ____ when being assigned to variables.',
-            choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-            answer: 'quotes',
-        },
-        {
-            title:
-                'A very useful tool used during development and debugging for printing content to the debugger is:',
-            choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
-            answer: 'console.log',
-        },
-    ];
+for (var i = 0; i < ScreenOrientation.length; i++) {
+    var score = document.createElement("ol"); 
+    score.textContent 
+}
 
-//function to check if the answer is right or wrong, current index++
-//
+//array that holds all of the questions, choices and answer
